6d54767fcc3d6ba0c073cbd753b4bdfe8a1297d0
Angel S Islas <isl03002@gmail.com>
2018-04-24 20:43:24 -0700

    Initial commit

---
+++ HW02_IslasAngel/ChangeDue.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace HW02_IslasAngel
+{
+    partial class HW02_IslasAngel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountGroupBox = new System.Windows.Forms.GroupBox();
+            this.tenderedAmountTextBox = new System.Windows.Forms.TextBox();
+            this.amountDueTextBox = new System.Windows.Forms.TextBox();
+            this.tenderedAmountLabel = new System.Windows.Forms.Label();
+            this.amountDueLabel = new System.Windows.Forms.Label();
+            this.changeGroupBox = new System.Windows.Forms.GroupBox();
+            this.centsLabel2 = new System.Windows.Forms.Label();
+            this.dimesLabel2 = new System.Windows.Forms.Label();
+            this.quartersLabel2 = new System.Windows.Forms.Label();
+            this.halfDollarsLabel2 = new System.Windows.Forms.Label();
+            this.dollarsLabel2 = new System.Windows.Forms.Label();
+            this.centsLabel = new System.Windows.Forms.Label();
+            this.dimesLabel = new System.Windows.Forms.Label();
+            this.quartersLabel = new System.Windows.Forms.Label();
+            this.halfDollarsLabel = new System.Windows.Forms.Label();
+            this.dollarsLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.amountGroupBox.SuspendLayout();
+            this.changeGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // amountGroupBox
+            // 
+            this.amountGroupBox.Controls.Add(this.tenderedAmountTextBox);
+            this.amountGroupBox.Controls.Add(this.amountDueTextBox);
+            this.amountGroupBox.Controls.Add(this.tenderedAmountLabel);
+            this.amountGroupBox.Controls.Add(this.amountDueLabel);
+            this.amountGroupBox.Location = new System.Drawing.Point(25, 40);
+            this.amountGroupBox.Name = "amountGroupBox";
+            this.amountGroupBox.Size = new System.Drawing.Size(577, 142);
+            this.amountGroupBox.TabIndex = 1;
+            this.amountGroupBox.TabStop = false;
+            // 
+            // tenderedAmountTextBox
+            // 
+            this.tenderedAmountTextBox.Location = new System.Drawing.Point(308, 67);
+            this.tenderedAmountTextBox.Name = "tenderedAmountTextBox";
+            this.tenderedAmountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tenderedAmountTextBox.TabIndex = 5;
+            // 
+            // amountDueTextBox
+            // 
+            this.amountDueTextBox.Location = new System.Drawing.Point(20, 67);
+            this.amountDueTextBox.Name = "amountDueTextBox";
+            this.amountDueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.amountDueTextBox.TabIndex = 3;
+            // 
+            // tenderedAmountLabel
+            // 
+            this.tenderedAmountLabel.AutoSize = true;
+            this.tenderedAmountLabel.Location = new System.Drawing.Point(305, 32);
+            this.tenderedAmountLabel.Name = "tenderedAmountLabel";
+            this.tenderedAmountLabel.Size = new System.Drawing.Size(92, 13);
+            this.tenderedAmountLabel.TabIndex = 4;
+            this.tenderedAmountLabel.Text = "Tendered Amount";
+            // 
+            // amountDueLabel
+            // 
+            this.amountDueLabel.AutoSize = true;
+            this.amountDueLabel.Location = new System.Drawing.Point(17, 32);
+            this.amountDueLabel.Name = "amountDueLabel";
+            this.amountDueLabel.Size = new System.Drawing.Size(66, 13);
+            this.amountDueLabel.TabIndex = 3;
+            this.amountDueLabel.Text = "Amount Due";
+            // 
+            // changeGroupBox
+            // 
+            this.changeGroupBox.AccessibleName = "-";
+            this.changeGroupBox.Controls.Add(this.centsLabel2);
+            this.changeGroupBox.Controls.Add(this.dimesLabel2);
+            this.changeGroupBox.Controls.Add(this.quartersLabel2);
+            this.changeGroupBox.Controls.Add(this.halfDollarsLabel2);
+            this.changeGroupBox.Controls.Add(this.dollarsLabel2);
+            this.changeGroupBox.Controls.Add(this.centsLabel);
+            this.changeGroupBox.Controls.Add(this.dimesLabel);
+            this.changeGroupBox.Controls.Add(this.quartersLabel);
+            this.changeGroupBox.Controls.Add(this.halfDollarsLabel);
+            this.changeGroupBox.Controls.Add(this.dollarsLabel);
+            this.changeGroupBox.Location = new System.Drawing.Point(25, 221);
+            this.changeGroupBox.Name = "changeGroupBox";
+            this.changeGroupBox.Size = new System.Drawing.Size(577, 142);
+            this.changeGroupBox.TabIndex = 2;
+            this.changeGroupBox.TabStop = false;
+            this.changeGroupBox.Text = "Change";
+            // 
+            // centsLabel2
+            // 
+            this.centsLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centsLabel2.Location = new System.Drawing.Point(483, 82);
+            this.centsLabel2.Name = "centsLabel2";
+            this.centsLabel2.Size = new System.Drawing.Size(37, 15);
+            this.centsLabel2.TabIndex = 10;
+            // 
+            // dimesLabel2
+            // 
+            this.dimesLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dimesLabel2.Location = new System.Drawing.Point(382, 82);
+            this.dimesLabel2.Name = "dimesLabel2";
+            this.dimesLabel2.Size = new System.Drawing.Size(37, 15);
+            this.dimesLabel2.TabIndex = 9;
+            // 
+            // quartersLabel2
+            // 
+            this.quartersLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.quartersLabel2.Location = new System.Drawing.Point(274, 82);
+            this.quartersLabel2.Name = "quartersLabel2";
+            this.quartersLabel2.Size = new System.Drawing.Size(37, 15);
+            this.quartersLabel2.TabIndex = 8;
+            // 
+            // halfDollarsLabel2
+            // 
+            this.halfDollarsLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.halfDollarsLabel2.Location = new System.Drawing.Point(146, 82);
+            this.halfDollarsLabel2.Name = "halfDollarsLabel2";
+            this.halfDollarsLabel2.Size = new System.Drawing.Size(37, 15);
+            this.halfDollarsLabel2.TabIndex = 7;
+            // 
+            // dollarsLabel2
+            // 
+            this.dollarsLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dollarsLabel2.Location = new System.Drawing.Point(17, 82);
+            this.dollarsLabel2.Name = "dollarsLabel2";
+            this.dollarsLabel2.Size = new System.Drawing.Size(37, 15);
+            this.dollarsLabel2.TabIndex = 6;
+            // 
+            // centsLabel
+            // 
+            this.centsLabel.AutoSize = true;
+            this.centsLabel.Location = new System.Drawing.Point(480, 32);
+            this.centsLabel.Name = "centsLabel";
+            this.centsLabel.Size = new System.Drawing.Size(34, 13);
+            this.centsLabel.TabIndex = 6;
+            this.centsLabel.Text = "Cents";
+            // 
+            // dimesLabel
+            // 
+            this.dimesLabel.AutoSize = true;
+            this.dimesLabel.Location = new System.Drawing.Point(379, 32);
+            this.dimesLabel.Name = "dimesLabel";
+            this.dimesLabel.Size = new System.Drawing.Size(36, 13);
+            this.dimesLabel.TabIndex = 5;
+            this.dimesLabel.Text = "Dimes";
+            // 
+            // quartersLabel
+            // 
+            this.quartersLabel.AutoSize = true;
+            this.quartersLabel.Location = new System.Drawing.Point(271, 32);
+            this.quartersLabel.Name = "quartersLabel";
+            this.quartersLabel.Size = new System.Drawing.Size(47, 13);
+            this.quartersLabel.TabIndex = 3;
+            this.quartersLabel.Text = "Quarters";
+            // 
+            // halfDollarsLabel
+            // 
+            this.halfDollarsLabel.AutoSize = true;
+            this.halfDollarsLabel.Location = new System.Drawing.Point(143, 32);
+            this.halfDollarsLabel.Name = "halfDollarsLabel";
+            this.halfDollarsLabel.Size = new System.Drawing.Size(61, 13);
+            this.halfDollarsLabel.TabIndex = 4;
+            this.halfDollarsLabel.Text = "Half Dollars";
+            // 
+            // dollarsLabel
+            // 
+            this.dollarsLabel.AutoSize = true;
+            this.dollarsLabel.Location = new System.Drawing.Point(17, 32);
+            this.dollarsLabel.Name = "dollarsLabel";
+            this.dollarsLabel.Size = new System.Drawing.Size(39, 13);
+            this.dollarsLabel.TabIndex = 3;
+            this.dollarsLabel.Text = "Dollars";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(527, 397);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(268, 397);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 12;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(407, 397);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 13;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // HW02_IslasAngel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.changeGroupBox);
+            this.Controls.Add(this.amountGroupBox);
+            this.Controls.Add(this.label1);
+            this.Name = "HW02_IslasAngel";
+            this.Text = "Change Due";
+            this.amountGroupBox.ResumeLayout(false);
+            this.amountGroupBox.PerformLayout();
+            this.changeGroupBox.ResumeLayout(false);
+            this.changeGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox amountGroupBox;
+        private System.Windows.Forms.TextBox tenderedAmountTextBox;
+        private System.Windows.Forms.TextBox amountDueTextBox;
+        private System.Windows.Forms.Label tenderedAmountLabel;
+        private System.Windows.Forms.Label amountDueLabel;
+        private System.Windows.Forms.GroupBox changeGroupBox;
+        private System.Windows.Forms.Label centsLabel2;
+        private System.Windows.Forms.Label dimesLabel2;
+        private System.Windows.Forms.Label quartersLabel2;
+        private System.Windows.Forms.Label halfDollarsLabel2;
+        private System.Windows.Forms.Label dollarsLabel2;
+        private System.Windows.Forms.Label centsLabel;
+        private System.Windows.Forms.Label dimesLabel;
+        private System.Windows.Forms.Label quartersLabel;
+        private System.Windows.Forms.Label halfDollarsLabel;
+        private System.Windows.Forms.Label dollarsLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ HW02_IslasAngel/ChangeDue.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW02_IslasAngel
+{
+    public partial class HW02_IslasAngel : Form
+    {
+
+        public HW02_IslasAngel()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+
+
+        {
+            //Declaring value of coin
+
+            decimal dollar = 1;
+            decimal halfDollar = .50m;
+            decimal quarter = .25m;
+            decimal dime = .10m;
+            decimal cent = .01m;
+            decimal amountDue;
+            decimal tenderedAmount;
+            decimal changeDue;
+
+
+            //Calculate change due to customer
+            amountDue = decimal.Parse(amountDueTextBox.Text);
+            tenderedAmount = decimal.Parse(tenderedAmountTextBox.Text);
+            changeDue = tenderedAmount - amountDue;
+
+
+
+
+            //Finding out how many dollars are in changeDue
+            decimal dollarMath = changeDue / dollar;
+            int numOfDollars = (int)dollarMath;
+
+            //Getting the remainer amount to figure out how much is left after substracting dollars
+            decimal remainerAfterDollars = changeDue % dollar;
+
+            dollarsLabel2.Text = numOfDollars.ToString();
+
+
+
+
+
+            //Finding out how many halfDollars are in changeDue
+            decimal halfDollarMath = remainerAfterDollars / halfDollar;
+            int numOfHalfDollar = (int)halfDollarMath;
+
+            //Getting the remainer amount to figure out how much is left after substracting halfDollars
+            decimal remainerAfterHalfDollars = remainerAfterDollars % halfDollar;
+
+
+            halfDollarsLabel2.Text = numOfHalfDollar.ToString();
+
+
+
+            //Finding out how many quarters are in changeDue
+            decimal quarterMath = remainerAfterHalfDollars / quarter;
+            int numOfQuarters = (int)quarterMath;
+
+            //Getting the remainer amount to figure out how much is left after substracting quarters
+            decimal remainerAfterQuarters = remainerAfterHalfDollars % quarter;
+
+
+           quartersLabel2.Text = numOfQuarters.ToString();
+
+
+
+            //Finding out how many dimes are in the remainerAfterQuarters
+            decimal dimeMath = remainerAfterQuarters / dime;
+            int numOfDimes = (int)dimeMath;
+            //Getting the remainer amount to figure out whats left after substracting dimes
+            decimal remainerAfterDimes = remainerAfterQuarters % dime;
+
+
+            dimesLabel2.Text = numOfDimes.ToString();
+
+
+
+            // Finding out how many pennies are in the remainerAfterNickles
+            decimal pennyMath = remainerAfterDimes / cent;
+            int numOfPennies = (int)pennyMath;
+
+
+            centsLabel2.Text = numOfPennies.ToString();
+
+
+
+
+        }
+
+
+        //Clear text boxes
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+            amountDueTextBox.Text = "";
+            tenderedAmountTextBox.Text = "";
+            dollarsLabel2.Text = "";
+            halfDollarsLabel2.Text = "";
+            quartersLabel2.Text = "";
+            dimesLabel2.Text = "";
+            centsLabel2.Text = "";
+
+            
+
+        }
+
+        //Close form
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
+    
+
+
